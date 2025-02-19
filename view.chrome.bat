7b5a3bb36af55b1835e8ef484df3b38ce973206d
liuxilu <53636947+liuxilu@users.noreply.github.com>
2025-02-19 18:34:10 +0800

    Add files via upload

---
+++ view.chrome.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Google\Chrome\Application\Chrome.exe" "%~dp0view.html" --allow-file-access-from-files --disable-web-security
---
+++ view.edge.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" "%~dp0view.html" --allow-file-access-from-files --disable-web-security
